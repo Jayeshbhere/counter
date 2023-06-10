861f85ce44a7f700cfecfa576c963d789f947e6e
Jayeshbhere <79853968+Jayeshbhere@users.noreply.github.com>
2023-06-10 14:32:51 +0530

    added western central herbal

--- src/components/Bhavesh.jsx
+++ src/components/Bhavesh.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
  const Bhavesh=()=>{
     return (
-    <h1>AJAO BOSS DIKHA DUNGA</h1>);
+    <h1>WESTERN LINE ME GHAR CHAHIYE AJAO BOSS DIKHA DUNGA
+      CENTRAL LINE ME GHAR CHAHIYE AJAO BOSS DIKHA DUNGA
+      HERBAL LINE ME GHAR CHAHIYE AJAO BOSS DIKHA DUNGA
+    </h1>);
  }
  export default Bhavesh;
